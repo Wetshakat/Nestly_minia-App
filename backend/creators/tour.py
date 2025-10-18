0b4e8eb3189061cb6130bb314ed2cdf029cd3679
SaadTech360 <saadibrahimumar7@gmail.com>
2025-10-18 23:46:27 +0100

    update the attraction files to include image

--- backend/creators/tour.py
+++ backend/creators/tour.py
@@ -6,6 +6,7 @@ class AttractionSite(models.Model):
     "model to allow creator create tourism site"
     creator = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
     name = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='sites')
     description = models.TextField()
     longitude = models.DecimalField(max_digits=9,decimal_places=6,null=True,blank=True)
     latitude = models.DecimalField(max_digits=9,decimal_places=6,null=True,blank=True)
